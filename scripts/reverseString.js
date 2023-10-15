3c74c20e9a04cc2dd4c4783a580f3ce7542ca089
Nirzon Karmakar <nirzon47@gmail.com>
2023-10-16 00:13:52 +0530

    added reverse-string.html with dummy files and favicon

---
+++ scripts/reverseString.js
@@ -0,0 +1,28 @@
+function reverseString() {
+	const resultElement = document.getElementById('result')
+	const s = document.getElementById('string').value
+
+	if (s === '') {
+		resultElement.innerHTML = '&nbsp;'
+		return
+	}
+
+	res = s.split('')
+
+	let left = 0,
+		right = s.length
+
+	while (left < right) {
+		let temp = res[left]
+		res[left] = res[right]
+		res[right] = temp
+
+		left++
+		right--
+	}
+
+	res = res.join('')
+
+	resultElement.innerHTML = ''
+	resultElement.innerHTML = res
+}
